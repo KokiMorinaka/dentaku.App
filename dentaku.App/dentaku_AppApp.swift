6442e900d85a20e5be6e74be923a5538d10c1bec
森中恒希 <21bd212@andrew.ac.jp>
2022-12-27 18:24:32 +0900

    Initial Commit

---
+++ dentaku.App/dentaku_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dentaku_AppApp.swift
+//  dentaku.App
+//
+//  Created by 森中恒希 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct dentaku_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
